2fc32e8513b0d914faea43f0e8011c17e90938e7
tranchin123 <dangvantranchinh@gmail.com>
2024-09-17 16:54:25 +0700

    add onClick prop

--- bookshelter-app/src/components/BookCategory/index.tsx
+++ bookshelter-app/src/components/BookCategory/index.tsx
@@ -3,11 +3,16 @@ import styles from './index.module.css';
 interface BookCategoryProps {
   category: string;
   quantity: number;
+  onHandleClick: () => void;
 }
 
-const BookCategory = ({ category, quantity }: BookCategoryProps) => {
+const BookCategory = ({
+  category,
+  quantity,
+  onHandleClick,
+}: BookCategoryProps) => {
   return (
-    <div className={styles.categoryItem}>
+    <div className={styles.categoryItem} onClick={onHandleClick}>
       <div className={styles.initials}>{category.slice(0, 2)}</div>
       <p className={styles.category}>{category}</p>
       <p className={styles.bookQuantity}>{quantity}</p>
--- bookshelter-app/src/components/CategoryList/index.tsx
+++ bookshelter-app/src/components/CategoryList/index.tsx
@@ -1,13 +1,22 @@
 import styles from './index.module.css';
 import BookCategory from '../BookCategory';
+
 const CategoryList = () => {
+  const handleClick = () => {
+    console.log('abc');
+  };
+
   return (
     <>
       <section className={styles.categoryList}>
         <p className={styles.curatedList}>
           A curated list of every book ever written
         </p>
-        <BookCategory category="Adventure" quantity={345} />
+        <BookCategory
+          category="Adventure"
+          quantity={345}
+          onHandleClick={handleClick}
+        />
       </section>
     </>
   );
