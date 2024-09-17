4b0040e216c957f90d22638aa9d2d45b3da6e747
tranchin123 <dangvantranchinh@gmail.com>
2024-09-17 10:44:36 +0700

    implement CategoryItem component

--- bookshelter-app/src/components/CategoryItem/index.tsx
+++ bookshelter-app/src/components/CategoryItem/index.tsx
@@ -1,5 +1,18 @@
-const CateGoryItem = () => {
-  return <></>;
+import styles from './index.module.css';
+interface CateGoryItemProps {
+  category: string;
+  quantity: number;
+}
+const CateGoryItem = ({ category, quantity }: CateGoryItemProps) => {
+  return (
+    <>
+      <div className={styles.categoryItem}>
+        <div className={styles.initials}>{category.slice(0, 2)}</div>
+        <p className={styles.category}>{category}</p>
+        <p className={styles.bookQuantity}>{quantity}</p>
+      </div>
+    </>
+  );
 };
 
 export default CateGoryItem;
