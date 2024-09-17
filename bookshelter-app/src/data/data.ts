f1d6b71ff7cd4da0822feeda87b75aef5d3cff56
tranchin123 <dangvantranchinh@gmail.com>
2024-09-17 08:46:43 +0700

    create data.ts

---
+++ bookshelter-app/src/data/data.ts
@@ -0,0 +1,33 @@
+export const sculptureList = [
+  {
+    name: 'Homenaje a la Neurocirugía',
+    author: 'Marta Colvin Andrade',
+    published: 2021,
+    description:
+      'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+    url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  },
+  {
+    name: 'Floralis Genérica',
+    author: 'Eduardo Catalano',
+    description:
+      'This enormous (75 ft. or 23m) silver flower is located in Buenos Aires. It is designed to move, closing its petals in the evening or when strong winds blow and opening them in the morning.',
+    url: 'https://i.imgur.com/ZF6s192m.jpg',
+  },
+  {
+    name: 'Eternal Presence',
+    author: 'John Woodrow Wilson',
+    published: 2021,
+    description:
+      'Wilson was known for his preoccupation with equality, social justice, as well as the essential and spiritual qualities of humankind. This massive (7ft. or 2,13m) bronze represents what he described as "a symbolic Black presence infused with a sense of universal humanity."',
+    url: 'https://i.imgur.com/aTtVpES.jpg',
+  },
+  {
+    name: 'Moai',
+    author: 'Unknown Artist',
+    published: 2021,
+    description:
+      'Located on the Easter Island, there are 1,000 moai, or extant monumental statues, created by the early Rapa Nui people, which some believe represented deified ancestors.',
+    url: 'https://i.imgur.com/RCwLEoQm.jpg',
+  },
+];
