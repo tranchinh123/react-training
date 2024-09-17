55146b084789010358c89325728a363ae23cb6e4
tranchin123 <dangvantranchinh@gmail.com>
2024-09-17 09:35:20 +0700

    implement BookCard component

--- bookshelter-app/src/pages/Home/index.tsx
+++ bookshelter-app/src/pages/Home/index.tsx
@@ -1,12 +1,19 @@
 import Header from '../../components/Header';
 import CategoriesSection from '../../components/CategoriesSection';
-import CategoryList from '../../components/CategoryList';
+// import CategoryList from '../../components/CategoryList';
+import BookCard from '../../components/BookCard';
 const HomePage = () => {
   return (
     <>
       <Header />
       <CategoriesSection />
-      <CategoryList />
+      {/* <CategoryList /> */}
+      <BookCard
+        name="Angels and demons"
+        author="Stuart Matt"
+        published={2021}
+        url="https://i.imgur.com/Mx7dA2Y.jpg"
+      />
     </>
   );
 };
