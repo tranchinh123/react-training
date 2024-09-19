9231b61eb46c06cf01f14490b6f6dcff7f561d1b
tranchin123 <dangvantranchinh@gmail.com>
2024-09-19 11:21:26 +0700

    code UI bookCardList

--- bookshelter-app/src/pages/Home/index.tsx
+++ bookshelter-app/src/pages/Home/index.tsx
@@ -1,16 +1,11 @@
 import DefaultLayout from '../../layouts';
-import BookCard from '../../components/BookCard';
+import BookCardList from '../../components/BookCardList';
 
 const HomePage = () => {
   return (
     <>
       <DefaultLayout>
-        <BookCard
-          name="Angels and demons"
-          author="Stuart Matt"
-          published={2021}
-          url="https://i.imgur.com/Mx7dA2Y.jpg"
-        ></BookCard>
+        <BookCardList />
       </DefaultLayout>
     </>
   );
