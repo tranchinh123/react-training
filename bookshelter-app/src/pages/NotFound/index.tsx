7dc799412d5b8cf37bb10d553eb8b4290f9e433f
tranchinh123 <dangvantranchinh@gmail.com>
2024-09-22 21:24:24 +0700

    implement UI NotFound page

--- bookshelter-app/src/pages/NotFound/index.tsx
+++ bookshelter-app/src/pages/NotFound/index.tsx
@@ -1,4 +1,14 @@
+import DefaultLayout from '../../layouts';
+import Glass from '../../components/Icons/Glass';
+import styles from './index.module.css';
 const NotFound = () => {
-  return <h1>NotFound Page</h1>;
+  return (
+    <DefaultLayout>
+      <div className={styles.NotFoundPage}>
+        <Glass size={39} />
+        <p className={styles.text}>No results found</p>
+      </div>
+    </DefaultLayout>
+  );
 };
 export default NotFound;
