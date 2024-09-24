c7bb97f28729045d906ceeb9ea3f604fea182813
tranchin123 <dangvantranchinh@gmail.com>
2024-09-24 15:36:40 +0700

    implement HeaderContentSection, InfoContentSection, MainContentSection component

---
+++ bookshelter-app/src/components/MainContentSection/index.tsx
@@ -0,0 +1,20 @@
+import styles from './index.module.css';
+import { Book } from '../../types';
+
+interface MainContentSectionProp {
+  book: Book | null;
+}
+const MainContentSection = ({ book }: MainContentSectionProp) => {
+  return (
+    <div className={styles.mainContent}>
+      <img
+        className={styles.imgBook}
+        src={book?.cover}
+        alt="image of the book "
+      />
+      <p className={styles.description}>{book?.description}</p>
+    </div>
+  );
+};
+
+export default MainContentSection;
