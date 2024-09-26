405206a6beaed6d3f63f8564d05811b3b3367241
tranchin123 <dangvantranchinh@gmail.com>
2024-09-26 14:58:54 +0700

    move handle logic in SearchInput component to Header

--- bookshelter-app/src/components/Header/index.tsx
+++ bookshelter-app/src/components/Header/index.tsx
@@ -2,11 +2,46 @@ import styles from './index.module.css';
 import Logo from '../Logo';
 import SearchInput from '../SearchInput';
 import SearchBookItem from '../SearchBookItem';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Book } from '../../types';
+import { useNavigate } from 'react-router-dom';
+import { get } from '../../services/api';
+import { API } from '../../constants/api';
+
 const Header = () => {
   const [results, setResults] = useState<Book[]>([]);
   const [isSearchOpen, setIsSearchOpen] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchBookList = async (): Promise<void> => {
+      const books = await get<Book[]>(API.BOOKS_ENDPOINT);
+      if (books && searchTerm.trim() !== '') {
+        const results = books.filter((books) => {
+          return books.title && books.title.toLowerCase().includes(searchTerm);
+        });
+        setResults(results);
+      }
+    };
+
+    const timeoutId = setTimeout(() => {
+      fetchBookList();
+    }, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchTerm, setResults]);
+
+  const handleChange = (value: string) => {
+    setSearchTerm(value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleClose();
+      navigate(`/name/${searchTerm}`);
+    }
+  };
 
   const handleOpen = () => {
     setIsSearchOpen(true);
@@ -22,9 +57,10 @@ const Header = () => {
         <Logo />
         <div className={styles.searchBarContainer}>
           <SearchInput
-            setResults={setResults}
+            searchTerm={searchTerm}
             onOpen={handleOpen}
-            onclose={handleClose}
+            handleKeyDown={handleKeyDown}
+            handleChange={handleChange}
           />
 
           {isSearchOpen && (
