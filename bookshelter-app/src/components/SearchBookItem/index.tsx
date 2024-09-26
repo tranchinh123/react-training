bf1e9d07844a6dbdb15b723e0d9ab0a27c865dbb
tranchin123 <dangvantranchinh@gmail.com>
2024-09-26 17:08:12 +0700

    When result return [] render UI to show user

--- bookshelter-app/src/components/SearchBookItem/index.tsx
+++ bookshelter-app/src/components/SearchBookItem/index.tsx
@@ -11,20 +11,24 @@ const SearchBookItem = ({ results, onClose }: SearchBookItemProps) => {
 
   return (
     <div className={styles.resultList}>
-      {results.map((result, id) => (
-        <div
-          key={id}
-          className={styles.bookItem}
-          id={result.id}
-          onClick={() => {
-            onClose();
-            navigate(`/detail/${result.id}`);
-          }}
-        >
-          <img className={styles.image} src={result.cover} alt="" />
-          <p className={styles.title}>{result.title}</p>
-        </div>
-      ))}
+      {results.length === 0 ? (
+        <p className={styles.noResults}>Không tìm thấy</p> // Hiển thị thông báo khi không có kết quả
+      ) : (
+        results.map((result, id) => (
+          <div
+            key={id}
+            className={styles.bookItem}
+            id={result.id}
+            onClick={() => {
+              onClose();
+              navigate(`/detail/${result.id}`);
+            }}
+          >
+            <img className={styles.image} src={result.cover} alt="" />
+            <p className={styles.title}>{result.title}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
