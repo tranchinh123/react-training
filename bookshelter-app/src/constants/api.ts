7b2bbec04deaa783ee940a37da68dd0fd8073341
tranchin123 <dangvantranchinh@gmail.com>
2024-10-02 09:57:53 +0700

    use file .env

--- bookshelter-app/src/constants/api.ts
+++ bookshelter-app/src/constants/api.ts
@@ -1,5 +1,6 @@
+const apiUrl = import.meta.env.VITE_API_URL;
 const API = {
-  BASE_URL: 'https://66e81ba3b17821a9d9db671e.mockapi.io/',
+  BASE_URL: apiUrl,
   BOOKS_ENDPOINT: 'books',
   CATEGORIES_ENDPOINT: 'categories',
 };
