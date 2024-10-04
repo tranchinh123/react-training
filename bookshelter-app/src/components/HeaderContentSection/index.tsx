9f6080f34ab239f807324e1484feafe3f84c984a
tranchinh123 <dangvantranchinh@gmail.com>
2024-10-04 08:41:06 +0700

    update back detail page

--- bookshelter-app/src/components/HeaderContentSection/index.tsx
+++ bookshelter-app/src/components/HeaderContentSection/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import LeftArrow from '../Icons/LeftArrow';
 import styles from './index.module.css';
 import { Book } from '../../types';
@@ -8,11 +8,17 @@ interface HeaderContentSectionProps {
 }
 
 const HeaderContentSection = ({ book }: HeaderContentSectionProps) => {
+  const navigate = useNavigate();
+  const handleBackClick = () => {
+    navigate(-1);
+  };
   return (
     <div className={styles.headerContentSection}>
       <Link to="/" className={styles.backLink}>
         <LeftArrow />
-        <span className={styles.backText}>Back</span>
+        <span className={styles.backText} onClick={handleBackClick}>
+          Back
+        </span>
       </Link>
       <p className={styles.nameBook}>{book.title}</p>
     </div>
