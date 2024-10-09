bd89642b9939863725151598761484eae9559f61
tranchin123 <dangvantranchinh@gmail.com>
2024-10-09 09:00:15 +0700

    update props bookcard

--- bookshelter-app/src/components/BookCard/index.tsx
+++ bookshelter-app/src/components/BookCard/index.tsx
@@ -1,31 +1,25 @@
 import styles from './index.module.css';
+import { Book } from '../../types';
 
 interface BookCardProps {
-  id: string;
-  cover: string;
-  title: string;
-  author: string;
-  publishedYear: number;
+  book: Book;
   onHandleClick: () => void;
 }
 
-const BookCard = ({
-  id,
-  cover,
-  title,
-  author,
-  publishedYear,
-  onHandleClick,
-}: BookCardProps) => {
+const BookCard = ({ book, onHandleClick }: BookCardProps) => {
   return (
-    <div className={styles.card} onClick={onHandleClick} id={id}>
+    <div className={styles.card} onClick={onHandleClick} id={book.id}>
       <div className={styles.imageCard}>
-        <img className={styles.imageBook} src={cover} alt="image of the book" />
+        <img
+          className={styles.imageBook}
+          src={book.cover}
+          alt="image of the book"
+        />
       </div>
       <div className={styles.bookInform}>
-        <h2 className={styles.nameBook}>{title}</h2>
-        <p className={styles.author}>{author}</p>
-        <p className={styles.published}>{publishedYear}</p>
+        <h2 className={styles.nameBook}>{book.title}</h2>
+        <p className={styles.author}>{book.author}</p>
+        <p className={styles.published}>{book.publishedYear}</p>
       </div>
     </div>
   );
