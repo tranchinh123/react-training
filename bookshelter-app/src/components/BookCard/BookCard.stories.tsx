4ea77abb5cb821b4cdfb217158efa229864b9103
tranchin123 <dangvantranchinh@gmail.com>
2024-11-08 17:31:30 +0700

    update stories for components

--- bookshelter-app/src/components/BookCard/BookCard.stories.tsx
+++ bookshelter-app/src/components/BookCard/BookCard.stories.tsx
@@ -24,14 +24,22 @@ const Template: StoryFn<BookCardArgs> = (args) => (
 export const Default = Template.bind({});
 Default.args = {
   book: {
-    id: '1',
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
-    publishedYear: 1925,
+    id: '63d7755f50f3cca31911ea67',
+    title: 'Nostrud commodo sint reprehenderit duis ad sit aute.',
+    author: 'Naomi',
+    category: 'contemporary',
+    publishedYear: 2016,
+    publisher: 'Liliana',
+    description:
+      'Veniam laborum sint magna in sit pariatur irure laboris anim et. Consequat ex duis sunt et labore consequat nisi. Ea amet officia ullamco nostrud officia irure duis nulla exercitation veniam sint ut. Culpa irure excepteur ea qui sit aliqua Lorem nulla ut minim culpa.\r\n',
     cover: 'https://i.ibb.co/ZSRxJHH/book-1.png',
-    category: 'Classic',
-    publisher: 'Scribner',
-    description: 'A novel set in the Roaring Twenties.',
+    comments: [
+      {
+        id: 1,
+        author: 'David',
+        comment: 'In dolore ad commodo proident sit commodo ad et in magna id.',
+      },
+    ],
   },
   onHandleClick: action('bookCard-clicked'),
 };
