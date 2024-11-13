be7c542d3f37dd5b7a2d0a60888a6a6f94f86c62
tranchin123 <dangvantranchinh@gmail.com>
2024-11-13 09:52:25 +0700

    apply code-splitting for Category List

--- bookshelter-app/src/layouts/index.tsx
+++ bookshelter-app/src/layouts/index.tsx
@@ -1,11 +1,12 @@
 import styles from './index.module.css';
 import Header from '../components/Header';
 import CategoriesSection from '../components/CategoriesSection';
-import CategoryList from '../components/CategoryList';
 import Loading from '../components/Loading';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, lazy, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import useFetchCategories from '../hooks/useFetchCategories';
+
+const CategoryList = lazy(() => import('../components/CategoryList'));
 
 const DefaultLayout = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,11 +23,14 @@ const DefaultLayout = () => {
     <>
       <Header onClick={handleClickMenu} />
       <CategoriesSection />
-      <CategoryList
-        categories={categories}
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={setIsMenuOpen}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CategoryList
+          categories={categories}
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
+        />
+      </Suspense>
+
       <section className={styles.content}>
         <Outlet />
       </section>
